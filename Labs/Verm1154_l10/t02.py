8352478fd7b86b03089ee3fb7dee6ead4e588de4
Himanya10 <145639852+Himanya10@users.noreply.github.com>
2024-04-27 23:55:51 -0400

    Add files via upload

---
+++ Labs/Verm1154_l10/t02.py
@@ -0,0 +1,16 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2024-03-14"
+-------------------------------------------------------
+"""
+from test_Sorts_array import test_sort
+from Sorts_array import Sorts
+
+a = [2, 3, 4, 1, 5, 6, 7]
+
+test_sort("Insertion Sort", Sorts.insertion_sort)
---
+++ Labs/Verm1154_l10/t04.py
@@ -0,0 +1,16 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2024-03-14"
+-------------------------------------------------------
+"""
+from test_Sorts_List_linked import test_sort
+from Sorts_List_linked import Sorts
+
+a = [2, 3, 4, 1, 5, 6, 7]
+
+test_sort("Insertion Sort", Sorts.insertion_sort)
